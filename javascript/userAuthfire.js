73ccc21d11e19fc933a2d48ef69fe4abf2b7d88f
Robert Diluzio Zobrist <zobrist.robert.480@gmail.com>
2017-06-09 17:50:32 -0700

    integration at 90% complete

--- javascript/userAuthfire.js
+++ javascript/userAuthfire.js
@@ -1,5 +1,3 @@
-
-
 // get elements
 const txtEmail = document.getElementById('email-input');
 const txtPassword = document.getElementById('password-input');
@@ -17,97 +15,118 @@ const scheduleMessageButton = document.getElementById('scheduleMessgeButton');
 
 
 // // initailize firebase \\
-  // Initialize Firebase
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAhCyPqOp9XfEGJvb1rFdqiDbQxL_ioi9o",
-    authDomain: "remindme-ea1a5.firebaseapp.com",
-    databaseURL: "https://remindme-ea1a5.firebaseio.com",
-    projectId: "remindme-ea1a5",
-    storageBucket: "remindme-ea1a5.appspot.com",
-    messagingSenderId: "831972778332"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+// Initialize Firebase
+var config = {
+     apiKey: "AIzaSyAhCyPqOp9XfEGJvb1rFdqiDbQxL_ioi9o",
+     authDomain: "remindme-ea1a5.firebaseapp.com",
+     databaseURL: "https://remindme-ea1a5.firebaseio.com",
+     projectId: "remindme-ea1a5",
+     storageBucket: "remindme-ea1a5.appspot.com",
+     messagingSenderId: "831972778332"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-     // add event-listenr to login button, and when clicked... \\
-     loginButton.addEventListener('click', e => {
+// add event-listenr to login button, and when clicked... \\
+loginButton.addEventListener('click', e => {
 
      // get email and password \\
      const email = txtEmail.value;
      const password = txtPassword.value;
+     let userPnumber = txtphoneNumber.value;
      const auth = firebase.auth();
+     console.log("target phone number---> " + userPnumber);
 
-     // sign in \\
-     const promise = auth.signInWithEmailAndPassword(email, password);
-
-     // clear email and password inputs \\
-     $("#email-input").val(" ");
-     $("#password-input").val(" ");
+     if (userPnumber.length === 10) {
 
 
-     promise.catch(e => console.log(e.message));
-})
+          // sign in \\
+          const promise = auth.signInWithEmailAndPassword(email, password);
 
-signupButton.addEventListener('click', e => {
-  
-          // get email an password \\
-          const email = txtEmail.value;
-          const password = txtPassword.value;
-          const phoneNumber = txtphoneNumber.value;
-          const username = txtUsername.value;
-          const auth = firebase.auth();
+          // clear email and password inputs \\
+          $("#email-input").val("");
+          $("#password-input").val("");
+          $("#phone-number-input").val("");
 
-
-          
-          
-
-          // create user and sign user in \\
-          const promise = auth.createUserWithEmailAndPassword(email, password);
 
           promise.catch(e => console.log(e.message));
-          // store related variables in object \\
-        var user = {
-             
-             email: email,
-             number: phoneNumber,
-             username: username,
-             password: password
+     } else {
+          $('#message').text("Please enter a valid Phonenumber to send an SMS to - example: 456-777-8899").css({'color' :'red'});
+          $("#email-input").val("");
+          $("#password-input").val("");
+          $("#phone-number-input").val("");
 
-        }
-     
-        // push user object to database, refrencing username as child_node name \\
-        database.ref(username).push(user);
- 
 
- 
-  });     
+     }
+});
+
+signupButton.addEventListener('click', e => {
+
+     // get email an password \\
+     const email = txtEmail.value;
+     const password = txtPassword.value;
+     const phoneNumber = txtphoneNumber.value;
+     const username = txtUsername.value;
+     const auth = firebase.auth();
+
+
+
+
+     // create user and sign user in \\
+     const promise = auth.createUserWithEmailAndPassword(email, password);
+
+     promise.catch(e => console.log(e.message));
+     // store related variables in object \\
+     var user = {
+
+          email: email,
+          number: phoneNumber,
+          username: username,
+          password: password
+
+     }
+
+     // push user object to database, refrencing username as child_node name \\
+     database.ref(username).push(user);
+
+     console.log("------>" + user.number);
+
+     this.phoneNumber = user.number;
+
+
+
+});
 // Upon new entry to database, update html with new data \\
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
      // Store everything into a variable.
      const userName = childSnapshot.val().name;
      const destination = childSnapshot.val().number;
-     const sms = childSnapshot.val().textMessage;
 
 
-    }); 
+});
 
 
 // add real-time  listener for user logged-in status 
 firebase.auth().onAuthStateChanged(firebaseUser => {
      // if user signed in
      if (firebaseUser) {
-          console.log(firebaseUser);
-          console.log("logged in");
-          // hide unneeded login and signup features 
-          $("#messaging-container, #email-input, #password-input, #name-input, #username-input, #login-button, #signup-button, #phone-number-input, #formText, #login-container, #register-container").hide();
-          // show logout button
-          $("#logout-button, #scheduleMessageButton, #messaging-container").show();
-          
+          database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+                    // Store everything into a variable.
+                    const userName = childSnapshot.val().name;
+                    const destination = childSnapshot.val().number;
+
+                    console.log("logged in");
+                    // hide unneeded login and signup features 
+                    $("#messaging-container, #email-input, #password-input, #name-input, #username-input, #login-button, #signup-button, #phone-number-input, #formText, #login-container, #register-container").hide();
+                    // show logout button and messaging feautures 
+                    $("#logout-button, #scheduleMessageButton, #messaging-container").show();
+                    $("#message").text(" ");
 
 
-          // do nothing until user logs in or signs up
+               })
+               // do nothing until user logs in or signs up
      } else {
           console.log('not logged in');
           // show needed input field
@@ -124,5 +143,3 @@ logoutButton.addEventListener('click', e => {
      $("#login-button, #email-input, #password-input, #formText1, #formText, #signup-button").show();
      $("#email-input, #password-input, #phone-number-input, #username-input, #name-input").text(" ");
 });
-
-
